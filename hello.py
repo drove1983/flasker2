729a093ff93abeba684832a3b4604cd306ae9dc7
Vedran Batinić <drove1983@gmail.com>
2023-10-26 18:02:07 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a raout decorator
+@app.route('/')
+
+#def index():
+	#return "<h1>Hello World!</h1>"
+
+def index():
+	first_name = "Vedran"
+	stuff = "This is <strong>Bold</strong> Text"
+	favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
+	return render_template("index.html",
+	 first_name=first_name,
+	 stuff=stuff,
+	 favorite_pizza=favorite_pizza)	
+
+#localhost:5000/user/John
+@app.route('/user/<name>')
+
+def user(name):
+	return render_template("user.html", user_name=name)	
+
+# Create Custom Error Pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+# Interval Server Error
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template("500.html"), 500				
